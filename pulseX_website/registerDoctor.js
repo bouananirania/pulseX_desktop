0e1e609c0f116d255cb444f3bb1e7b1b70c32636
bouananirania <138144709+bouananirania@users.noreply.github.com>
2024-03-18 02:02:34 +0100

    Update registerDoctor.js

--- pulseX_website/registerDoctor.js
+++ pulseX_website/registerDoctor.js
@@ -7,31 +7,38 @@ const { doctorDB } = require('../db');
 async function registerDoctor(req, res) {
   try {
     const { fullName, email, age, wilaya, phone, specialite, password, confirmPassword } = req.body;
+
     // Vérifier si le mot de passe et le mot de passe de confirmation sont identiques
     if (password !== confirmPassword) {
       return res.status(400).send("Les mots de passe ne correspondent pas");
     }
-    // Utiliser la connexion à la base de données des médecins depuis db.js
-    await doctorDB.then(() => {
-      // Vérifier si le docteur existe déjà dans la base de données
-      Doctor.findOne({ email }, async (err, existingDoctor) => {
+
+    // Attendre que la connexion à la base de données des médecins soit établie
+    await new Promise((resolve) => {
+      doctorDB.once('connected', () => {
+        resolve();
+      });
+    });
+
+    // Vérifier si le docteur existe déjà dans la base de données
+    Doctor.findOne({ email }, async (err, existingDoctor) => {
+      if (err) {
+        return res.status(500).send("Erreur lors de la recherche du docteur existant");
+      }
+      if (existingDoctor) {
+        return res.status(400).send("Le docteur existe déjà");
+      }
+      
+      // Hacher le mot de passe avant de l'enregistrer
+      const hashedPassword = await bcrypt.hash(password, 10);
+      
+      // Créer un nouveau docteur avec Mongoose
+      const newDoctor = new Doctor({ fullName, email, age, wilaya, phone, specialite, password: hashedPassword });
+      await newDoctor.save((err, doc) => {
         if (err) {
-          return res.status(500).send("Erreur lors de la recherche du docteur existant");
+          return res.status(500).send("Erreur lors de l'enregistrement du nouveau docteur");
         }
-        if (existingDoctor) {
-          return res.status(400).send("Le docteur existe déjà");
-        }
-        // Hacher le mot de passe avant de l'enregistrer
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const hashedconfirmPassword = await bcrypt.hash(confirmPassword, 10);
-        // Créer un nouveau docteur avec Mongoose
-        const newDoctor = new Doctor({ fullName, email, age, wilaya, phone, specialite, password: hashedPassword, confirmPassword: hashedconfirmPassword });
-        await newDoctor.save((err, doc) => {
-          if (err) {
-            return res.status(500).send("Erreur lors de l'enregistrement du nouveau docteur");
-          }
-          res.status(201).send("Inscription réussie");
-        });
+        res.status(201).send("Inscription réussie");
       });
     });
   } catch (err) {
