7baa3834299d65e021d8484f4e3ca4cd1f0ca6a2
bouananirania <138144709+bouananirania@users.noreply.github.com>
2024-03-24 23:26:09 +0100

    Update doctorRoutes.js

--- routes/doctorRoutes.js
+++ routes/doctorRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const doctorController = require('../controllers/doctorController');
 
 // Route pour la connexion du médecin
-router.post('/login', doctorController.loginDoctor);
+router.post('/logindr', doctorController.loginDoctor);
 
 // Route pour obtenir les paramètres du médecin
-router.get('/settings', doctorController.getDoctorSettings);
+router.get('/settingsdr', doctorController.getDoctorSettings);
 
 module.exports = router;
