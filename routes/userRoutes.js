a7ed790daa716db8fd89ed8e47b73ec8adcb2c9f
chialimouad <159821581+chialimouad@users.noreply.github.com>
2024-03-26 02:54:28 +0100

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-
+const dbq=require('../models/uUer')
 // Route pour créer un nouvel utilisateur
 router.post('/addPatient', userController.createUser);
 
@@ -10,5 +10,12 @@ router.delete('/:userId', userController.deleteUser);
 
 // Route pour mettre à jour un utilisateur
 router.put('/:userId', userController.updateUser);
-
+route.get('/fetch',async (req,res)=>{
+  try{
+    const fetch =await dbq.find({})
+    res.status(200).json(fetch)
+    
+  }catch(err){console.log(err)}
+  
+})
 module.exports = router;
