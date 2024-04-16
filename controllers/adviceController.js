a40d5624e2ab73cde244b88dc69fd74cce2ecd69
bouananirania <138144709+bouananirania@users.noreply.github.com>
2024-04-16 01:40:13 +0100

    Update adviceController.js

--- controllers/adviceController.js
+++ controllers/adviceController.js
@@ -9,4 +9,10 @@ exports.registeradvice= async(req,res,next)=>{
     res.json({status:true,success:"user succsefully"});
     
   }catch(err){console.log(err)}};
-  
+
+  exports.getadvice=async(req,res)=>{
+    const {}=req.body;
+    try
+   { const getadv=await Advice.find({});
+        return getadv;  res.json({status:true,success:getdatafrom})
+    }catch(err){console.log(err)};}
