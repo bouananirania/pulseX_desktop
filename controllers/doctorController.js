ff3305b54020248c499de1712696c1c93d46b399
bouananirania <138144709+bouananirania@users.noreply.github.com>
2024-04-16 01:59:20 +0100

    Update doctorController.js

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -84,7 +84,7 @@ exports.updatedoctor= async(req,res)=>{
   const {id} = req.body;
   const {fullName,email,age,wilaya,phone,specialite,password,name} = req.body;
   try {
-  const data  = await Doctor.findByIdAndUpdate({_id},  {fullName,email,age,wilaya,phone,specialite,password,name}, { new: true });
+  const data  = await Doctor.findByIdAndUpdate(id,  {fullName,email,age,wilaya,phone,specialite,password,name}, { new: true });
       res.json({status:true,success:update})
 }catch(err){ return res.status(500).json({ message: ' server error', err: err.message });
 }};
@@ -92,7 +92,7 @@ exports.updatedoctor= async(req,res)=>{
 exports.deletedoc= async(req,res)=>{
   try{
   const {id}=req.body
-  const deleteDr = await User.findByIdAndDelete({_id});
+  const deleteDr = await User.findByIdAndDelete(id);
  res.json({status:true,success:deleteDr});
 }catch(err){console.log(err);}};
 
