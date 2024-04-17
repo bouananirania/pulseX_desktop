cb5c9f93a1cd1cfe4699269594a81b8b7ed8b17a
bouananirania <138144709+bouananirania@users.noreply.github.com>
2024-04-17 02:33:24 +0100

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 
-router.post('/addPatient', userController.createUser);//noresponse 
+router.post('/addPatient', userController.createUser);//checked
 router.delete('/deletuser', userController.deleteUser);
 router.put('/updateuser', userController.updateUser);
-router.get('/getuserdata',userController.getuserdata);
+router.get('/getuserdata',userController.getuserdata);//checked
 //recuperer les patients du DR
-router.get('/patients',userController.getPatientsByDoctorId);
-router.post('/finding',userController.finding);
+router.get('/patients',userController.getPatientsByDoctorId);//checked
+router.get('/finding',userController.finding);
 
 module.exports = router;
